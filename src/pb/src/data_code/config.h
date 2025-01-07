ecbc70c3f1ecbd213c11ebc4b3a42f51a68950b8
printBIM22 <info@printBIM.ru>
2025-01-07 10:07:22 +0300

    Закончил на 22

--- src/pb/src/data_code/config.h
+++ src/pb/src/data_code/config.h
@@ -112,7 +112,7 @@ const uint16_t max_size_stuct3 = (size_structura_Data2Print < size_structura_Pri
 #define SIZE_BUFF_DRIVER 96            // Размер буфера, стараться делать кратно 32
 unsigned char bufferDriver[SIZE_BUFF_DRIVER]; // Буфер в 1 байт в который пишем передаваемый байт и в котором оказывется принятый байт
 
-#define SIZE_BUFF_PRINT 24            // Размер буфера, стараться делать кратно 32
+#define SIZE_BUFF_PRINT 32            // Размер буфера, стараться делать кратно 32
 unsigned char bufferPrint[SIZE_BUFF_PRINT]; // Буфер в 1 байт в который пишем передаваемый байт и в котором оказывется принятый байт
 
 #define SIZE_BUFF_MODUL 236            // Размер буфера, стараться делать кратно 32 для ESP, для STM непонятно пока
